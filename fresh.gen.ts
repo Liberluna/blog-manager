51ce76c32e5fc4d7d90b13ebed05ae60396e8614
nakasyou <syoutarou1103@outlook.jp>
2023-06-03 04:14:12 +0000

    gen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,18 +6,22 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/auth/[code].ts";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/dash.tsx";
+import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/HomeDash.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/auth/[code].ts": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/dash.tsx": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/HomeDash.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
