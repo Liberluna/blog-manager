87432b438eefc5f601d7f9899e64cd43f32bd2a9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 15:47:48 +0900

    Update HomeDash.tsx

--- islands/HomeDash.tsx
+++ islands/HomeDash.tsx
@@ -8,14 +8,11 @@ interface Props {
 export default function Counter(props: Props) {
   const [mode, setMode] = useState("new-post")
   
-  function newPost(){
-    setMode("new-post")
-  }
   
   return (
     <div>
       <a>新しい投稿を作成する</a>
-      <NewPost show={mode==="new-post"}/>
+      <NewPost/>
     </div>
   );
 }
