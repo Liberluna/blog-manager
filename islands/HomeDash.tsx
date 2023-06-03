1ef7c250783cba7a23f7e0d2ad176b85defc8d0f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 13:46:34 +0900

    Update HomeDash.tsx

--- islands/HomeDash.tsx
+++ islands/HomeDash.tsx
@@ -5,9 +5,22 @@ interface Props {
 }
 
 export default function Counter(props: Props) {
-  useEffect(()=>{
-    alert("a")
+  
+  useEffect(async ()=>{
+    function re(){
+      location.href = "https://github.com/login/oauth/authorize?scope=user:email&client_id=7ad00edcde8a0a74d3e6"
+    }
+    const params = new URLSearchParams(location.search)
+    
+    if(!params.has("code"))
+      re()
+   
+    const res = await fetch(`/api/auth/${params.get("code")}`)
+    const json = await res.json()
+    
+    alert(JSON.stringify(json))
   },[])
+  
   return (
     <div>
       home dash
