4304195da61ffda086b8522acfb3828bdab4c616
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 12:46:42 +0900

    Update [code].ts

--- routes/api/auth/[code].ts
+++ routes/api/auth/[code].ts
@@ -1,7 +1,11 @@
 import { HandlerContext } from "$fresh/server.ts";
 
-export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+export const handler = async  (_req: Request, _ctx: HandlerContext): Promise<Response> => {
   const { pathname } = new URL(_req.url);
-  const code = pathname.replace("/api/auth/","")
-  return new Response(code);
+  const code = pathname.replace("/api/auth/","")/
+  
+  const reqURL = `https://github.com/login/oauth/access_token?code=${code}&client_id=7ad00edcde8a0a74d3e6&client_secret=${Deno.env.get("client_secret")}`;
+  
+  
+  return new Response(reqURL);
 };
