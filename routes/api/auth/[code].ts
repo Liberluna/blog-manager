fa401a070425754896a71d6199ab6686aaf90d86
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 12:50:36 +0900

    fix syntac

--- routes/api/auth/[code].ts
+++ routes/api/auth/[code].ts
@@ -2,7 +2,7 @@ import { HandlerContext } from "$fresh/server.ts";
 
 export const handler = async  (_req: Request, _ctx: HandlerContext): Promise<Response> => {
   const { pathname } = new URL(_req.url);
-  const code = pathname.replace("/api/auth/","")/
+  const code = pathname.replace("/api/auth/","")
   
   const reqURL = `https://github.com/login/oauth/access_token?code=${code}&client_id=7ad00edcde8a0a74d3e6&client_secret=${Deno.env.get("client_secret")}`;
   
