6bb6560892fd519afed99bbb422863fcca963731
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 13:05:24 +0900

    Create dash.tsx

---
+++ routes/dash.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "$fresh/server.ts"
+
+export default function(props: PageProps) {
+  return <>
+    <div class="mx-10">
+      <div class="text-2xl">
+        Dash
+      </div>
+    </div>
+  </>
+}
