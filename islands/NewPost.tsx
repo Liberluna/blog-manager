43687a2337be412cc1bf12b7dfc2edd437195fc1
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 15:41:23 +0900

    Create NewPost.tsx

---
+++ islands/NewPost.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "preact/hooks";
+
+export default function Counter(props: Props) {
+  const [userData, setUserData] = useState("")
+  const 
+  useEffect(()=>{
+    const token = localStorage.getItem("gh-token")
+    const octokit = new Octokit({
+      auth: token,
+    })
+  },[])
+  
+  return (
+    <div>
+      <div>
+        <span>表示名</span>
+        <input/>
+      </div>
+      <div>
+        <span>ID</span>
+        <input/>
+      </div>
+      <div>
+        <span>アイコンのPath</span>
+        <input placeholder="nakasyou.png"/>
+      </div>
+      <div>
+        <textarea>
+          ここにMarkdownを入力...
+        </textarea>
+      </div>
+    </div>
+  );
+}
