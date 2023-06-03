b178ac12331f5c1f382f84bb992b4d740cf85041
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 15:14:13 +0900

    token

--- islands/HomeDash.tsx
+++ islands/HomeDash.tsx
@@ -9,35 +9,7 @@ export default function Counter(props: Props) {
   const [userData, setUserData] = useState("")
   
   useEffect(async ()=>{
-    function re(){
-      location.href = "https://github.com/login/oauth/authorize?scope=user:email&client_id=7ad00edcde8a0a74d3e6"
-    }
-    const params = new URLSearchParams(location.search)
-    
-    if(!params.has("code")){
-      re()
-      return
-    }
-    const res = await fetch(`/api/auth/${params.get("code")}`)
-    const json = await res.json()
-    
-    if(json.error === "error"){
-      re()
-      return
-    }
-    localStorage.setItem("gh-token", json.token)
-    
-    await (async()=>{
-      const octokit = new Octokit({
-        auth: json.token,
-      })
-      const res = await octokit.request('GET /user', {
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      })
-      setUserData(res)
-    })();
+    alert(localStrage.getItem("gh-token"))
   },[])
   
   return (
