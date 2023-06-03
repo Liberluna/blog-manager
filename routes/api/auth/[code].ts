fd848b4457b798ce9cb56faac2251c670d77e057
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 12:53:47 +0900

    apiTxt

--- routes/api/auth/[code].ts
+++ routes/api/auth/[code].ts
@@ -5,7 +5,10 @@ export const handler = async  (_req: Request, _ctx: HandlerContext): Promise<Res
   const code = pathname.replace("/api/auth/","")
   
   const reqURL = `https://github.com/login/oauth/access_token?code=${code}&client_id=7ad00edcde8a0a74d3e6&client_secret=${Deno.env.get("client_secret")}`;
+  const apiRes = await fetch(reqURL)
+  const apiTxt = await apiRes.text()
   
+  //const params = new URLSearchParams(apiTxt)
   
-  return new Response(reqURL);
+  return new Response(apiTxt);
 };
