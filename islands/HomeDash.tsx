36ec68d4823fd68b972af9a60c10e1a387b74315
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 13:12:07 +0900

    Create HomeDash.tsx

---
+++ islands/HomeDash.tsx
@@ -0,0 +1,14 @@
+import { useState } from "preact/hooks";
+
+interface Props {
+
+}
+
+export default function Counter(props: Props) {
+  alert("konnntiha")
+  return (
+    <div>
+      home dash
+    </div>
+  );
+}
