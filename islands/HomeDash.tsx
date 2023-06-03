8071fd98eed0f3e36dcb9e3aeef84a7b504a9ba2
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 15:37:20 +0900

    fix syntax

--- islands/HomeDash.tsx
+++ islands/HomeDash.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "preact/hooks";
-import { Octokit } from "octokit"
 
 interface Props {
 
@@ -8,24 +7,6 @@ interface Props {
 export default function Counter(props: Props) {
   const [userData, setUserData] = useState("")
   
-  useEffect(()=>{
-    try{
-      const token = localStorage.getItem("gh-token")
-      const window.octokit = new Octokit({
-        auth: token,
-      })
-      alert("inited octokit!")
-    }catch(e){
-      document.body.innerHTML += `<div style="position:fixed;background-color:#ddd"><div>
-        <span>${e.name}:</span>
-        <span>${e.message}</span>
-      </div>
-      <div>
-        ${e.stack.replaceAll("\n","<br>")}
-      </div></div>`
-      throw e
-    }
-  },[])
   
   return (
     <div>
