4e6292dcc69bc7160147e75c9b52478973ab0452
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 12:44:15 +0900

    code

--- routes/api/auth/[code].ts
+++ routes/api/auth/[code].ts
@@ -2,6 +2,6 @@ import { HandlerContext } from "$fresh/server.ts";
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
   const { pathname } = new URL(_req.url);
-  
-  return new Response(pathname);
+  const code = pathname.replace("/api/auth/","")
+  return new Response(code);
 };
